c767ce2b4e0572528f0abf2d628f372794ec888e
Gaby <gaby.boutayeh@protonmail.com>
2026-01-24 21:10:06 +0000

    pipeline test

---
+++ pipeline/pipeline.py
@@ -0,0 +1,18 @@
+import sys
+import pandas as pd
+
+print('arguments:', sys.argv)
+
+month = int(sys.argv[1]) if len(sys.argv) > 1 else 12
+
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month'] = month
+print(df.head())
+
+df.to_parquet(f"output_{month}.parquet", index=False)
+
+print(f"Processing data for month: {month}")
+
+
+
+
